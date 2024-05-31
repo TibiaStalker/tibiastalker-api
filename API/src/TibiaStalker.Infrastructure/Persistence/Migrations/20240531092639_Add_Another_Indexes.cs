cf66be2c5db79da7fd058310f694e8346b516f8b
kamiljanek <95696684+kamiljanek@users.noreply.github.com>
2024-05-31 11:27:51 +0200

    Add Another Indexes

---
+++ API/src/TibiaStalker.Infrastructure/Persistence/Migrations/20240531092639_Add_Another_Indexes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TibiaStalker.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnotherIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_world_scan_world_id_is_deleted_scan_date_time",
+                schema: "public",
+                table: "world_scans",
+                columns: new[] { "world_id", "is_deleted", "scan_create_date_time" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_correlations_number_matches_last_match_date",
+                schema: "public",
+                table: "character_correlations",
+                columns: new[] { "number_of_matches", "last_match_date" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_world_scan_world_id_is_deleted_scan_date_time",
+                schema: "public",
+                table: "world_scans");
+
+            migrationBuilder.DropIndex(
+                name: "ix_correlations_number_matches_last_match_date",
+                schema: "public",
+                table: "character_correlations");
+        }
+    }
+}
